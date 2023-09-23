8711ab3588a465d429b1451549fbe037b76830ef
mounika konda <konda2mounika@gmail.com>
2023-09-23 15:59:12 +0530

    0001

--- 03-ec2/ec2.tf
+++ 03-ec2/ec2.tf
@@ -14,3 +14,28 @@ resource "aws_instance" "demo" {
 output "public_ip" {
     value  = aws_instance.demo.public_ip
 }
+
+# Creates security group
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    description      = "SSH from INTERNET"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+}
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_konda_ssg_sg"
+  }
+}
