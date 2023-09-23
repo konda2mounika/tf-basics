8afa43d5f4ed7ad4751102f850fe9a4c0c508498
mounika konda <konda2mounika@gmail.com>
2023-09-23 19:32:43 +0530

    0001

---
+++ 06-ex2/ec2/main.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "demo1" {
+  ami                       = "ami-0cb7d8dc2b12a2c0d"
+  instance_type             = "t2.micro"
+  vpc_security_group_ids    = [var.sg]
+
+  tags     = {
+    Name   = "EC2-Terraform"
+  }
+
+ 
+
+variable "sg" {}
+
