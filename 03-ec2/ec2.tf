0322a6341718b484af1dbb56a2dbd258d6ca4e5b
mounika konda <konda2mounika@gmail.com>
2023-09-23 15:34:46 +0530

    0001

---
+++ 03-ec2/ec2.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "demo" {
+  ami                       = "ami-0cb7d8dc2b12a2c0d"
+  instance_type             = "t2.micro"
+
+
+  tags     = {
+    Name   = "EC2-Terraform-Instance"
+  }
+}
