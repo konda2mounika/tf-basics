7bcc52eb4095e2467ab852920f03d4a83ac191f9
mounika konda <konda2mounika@gmail.com>
2023-09-23 16:04:03 +0530

    0001

--- 03-ec2/ec2.tf
+++ 03-ec2/ec2.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "demo" {
   ami                       = "ami-0cb7d8dc2b12a2c0d"
   instance_type             = "t2.micro"
+  vpc_security_group_ids    = [aws_security_group.allow_ssh.id]
 
 
   tags     = {
