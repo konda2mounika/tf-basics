d5642f43021222cf54e0307afcdfd5fb5213a7d8
mounika konda <konda2mounika@gmail.com>
2023-09-24 00:56:47 +0530

    0001

--- python/01.py
+++ python/01.py
@@ -3,7 +3,7 @@
 # Importing boto3 library to make functionality available
 import boto3
 # Creating the connection with the resource of AWS EC2 service
- ec2 = boto3.resource('ec2')
+ ec2 = boto3.client('ec2')
 # creating a new EC2 instance
  instances = ec2.create_instances(
       ImageId='ami-0cb7d8dc2b12a2c0d',
