40e352a7f43c30fe7510e563095fffa99ba35cd7
mounika konda <konda2mounika@gmail.com>
2023-09-23 16:08:52 +0530

    0001

--- 03-ec2/ec2.tf
+++ 03-ec2/ec2.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "demo" {
   ami                       = "ami-0cb7d8dc2b12a2c0d"
   instance_type             = "t2.micro"
-  vpc_security_group_ids    = [aws_security_group.allow_KONDAssh.id]
+  vpc_security_group_ids    = [aws_security_group.allow_K_ssh.id]
 
 
   tags     = {
@@ -17,7 +17,7 @@ output "public_ip" {
 }
 
 # Creates security group
-resource "aws_security_group" "allow_KONDa_ssh" {
+resource "aws_security_group" "allow_K_ssh" {
   name        = "allow_ssh"
   description = "Allow SSH inbound traffic"
 
