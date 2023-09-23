ffee517f83e56d7357dc56298cd2c6dfef1d7230
mounika konda <konda2mounika@gmail.com>
2023-09-23 20:41:08 +0530

    0001

--- 06-ex2/ec2/main.tf
+++ 06-ex2/ec2/main.tf
@@ -7,7 +7,22 @@ resource "aws_instance" "demo1" {
     Name   = "EC2-Terraform"
   }
 }
- 
+   provisioner "remote-exec" {
+  
+  # Connection Provisioner
+  connection {
+    type     = "ssh"
+    user     = "centos"
+    password = "DevOps321"
+    host     = self.public_ip
+  }
+
+    inline = [
+      "ansible-pull -U https://github.com/b50-clouddevops/ansible.git -e COMPONENT=frontend -e ENV=dev -e APP_VERSION=0.0.2 roboshop-pull.yml",
+    ]
+  }
+
+}
 
 variable "sg" {}
 
