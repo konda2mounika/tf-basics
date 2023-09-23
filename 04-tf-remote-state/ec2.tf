01ac68d992a322c03d8227a2f5f58ef2445fb4fe
mounika konda <konda2mounika@gmail.com>
2023-09-23 16:33:33 +0530

    0001

--- 04-tf-remote-state/ec2.tf
+++ 04-tf-remote-state/ec2.tf
@@ -2,14 +2,14 @@ provider "aws" {
     region      = "us-east-1"
 }
 
-resource "aws_instance" "demo" {
+resource "aws_instance" "demo2" {
   ami                       = "ami-0cb7d8dc2b12a2c0d"
   instance_type             = "t2.micro"
-  vpc_security_group_ids    = [aws_security_group.allow_ssh.id]
+  vpc_security_group_ids    = [aws_security_group.allows_ssh.id]
 
 
   tags     = {
-    Name   = "EC2-Terraform-Instance"
+    Name   = "EC2-Terraform-Instance2"
   }
 }
 output "public_ip" {
@@ -17,8 +17,8 @@ output "public_ip" {
 }
 
 # Creates security group
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
+resource "aws_security_group" "allows_ssh" {
+  name        = "allows_ssh"
   description = "Allow SSH inbound traffic"
 
   ingress {
